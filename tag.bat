3b5f17854305ef3d797cbd1352637d77ed94b20e
Bobo <yanglinbo@gmail.com>
2025-12-22 11:18:10 +0800

    feat: fix bug.

--- tag.bat
+++ tag.bat
@@ -1,6 +1,6 @@
-﻿git tag v0.0.2
+﻿git tag v0.0.3
 
-git tag javascript/v0.0.2 --force
-git tag lua/v0.0.2 --force
+git tag javascript/v0.0.3 --force
+git tag lua/v0.0.3 --force
 
 git push origin --tags
